96bf488dec949d89eae4ad7db61048593de2ce0f
Alexander Ong <FireFox2000000@gmail.com>
2019-01-28 15:57:25 +1100

    Make sure anchors are process in copy constructor.

--- Moonscraper Chart Editor/Assets/Scripts/Controllers/Inspectors/BPMPropertiesPanelController.cs
+++ Moonscraper Chart Editor/Assets/Scripts/Controllers/Inspectors/BPMPropertiesPanelController.cs
@@ -296,7 +296,7 @@ public class BPMPropertiesPanelController : PropertiesPanelController {
 
             if (anchor == null || bpmToAdjust == currentBPM)
             {
-                commands.Add(new SongEditModify<BPM>(currentBPM, new BPM(currentBPM.tick, desiredBpmValue)));
+                commands.Add(new SongEditModify<BPM>(currentBPM, new BPM(currentBPM.tick, desiredBpmValue, currentBPM.anchor)));
                 return new BatchedSongEditCommand(commands);
             }
 
@@ -334,7 +334,7 @@ public class BPMPropertiesPanelController : PropertiesPanelController {
             {
                 if (newValue != 0)
                 {
-                    commands.Add(new SongEditModify<BPM>(bpmToAdjust, new BPM(bpmToAdjust.tick, newValue)));
+                    commands.Add(new SongEditModify<BPM>(bpmToAdjust, new BPM(bpmToAdjust.tick, newValue, bpmToAdjust.anchor)));
                 }
 
                 finalValue = desiredBpmValue;
@@ -346,7 +346,7 @@ public class BPMPropertiesPanelController : PropertiesPanelController {
         if (desiredBpmValue == currentBPM.value)
             return null;
 
-        commands.Add(new SongEditModify<BPM>(currentBPM, new BPM(currentBPM.tick, desiredBpmValue)));
+        commands.Add(new SongEditModify<BPM>(currentBPM, new BPM(currentBPM.tick, desiredBpmValue, currentBPM.anchor)));
         return new BatchedSongEditCommand(commands);
     }
 
